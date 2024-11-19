029694dd9c4d02547bf5a58e20f0ac84a4e2fe34
AhmedHassan-ux <ahmedhassan60306030@gmail.com>
2024-11-19 19:26:05 +0200

    first upload

---
+++ movies_database.js
@@ -0,0 +1,90 @@
+ moviesDatabase = [
+  {
+    "id": 912649,
+    "original_language": "en",
+    "release_date": "2024-10-22",
+    "backdrop_path": "/3V4kLQg0kSqPLctI5ziYWabAZYF.jpg",
+    "poster_path": "/vgKW7fYaxfgTmUFozMMABoLIxg0.jpg",
+    "genres": [
+      {
+        "id": 878,
+        "name": "خيال علمي"
+      },
+      {
+        "id": 28,
+        "name": "حركة"
+      },
+      {
+        "id": 12,
+        "name": "مغامرة"
+      }
+    ],
+    "title": "فينوم: الرقصة الأخيرة",
+    "overview": "يصبح (إيدي) و(فينوم) مطاردين من قبل العديد من الجهات، ويتعين عليهما اتخاذ قرارًا مدمرًا في المغامرة الأخيرة من رحلتهما معًا.",
+    "cast": [
+      {
+        "name": "Tom Hardy",
+        "profile_path": "/d81K0RH8UX7tZj49tZaQhZ9ewH.jpg"
+      },
+      {
+        "name": "Chiwetel Ejiofor",
+        "profile_path": "/kq5DDnqqofoRI0t6ddtRlsJnNPT.jpg"
+      },
+      {
+        "name": "Juno Temple",
+        "profile_path": "/wMpZcKp7zaHnmNQooqbve33577Q.jpg"
+      },
+      {
+        "name": "Clark Backo",
+        "profile_path": "/d24KKFxfoql6PBsBPsejFgzhSlH.jpg"
+      },
+      {
+        "name": "Rhys Ifans",
+        "profile_path": "/1D670EEsbky3EtO7XLG32A09p92.jpg"
+      }
+    ],
+    "custom_link": "https://w5.gameshop4u.com/embed-zyfoiekasnss.html"
+  },
+  {
+    "id": 1125510,
+    "original_language": "es",
+    "release_date": "2024-09-27",
+    "backdrop_path": "/epQfhIcfqWgrMhRvnxkVm7EeEpE.jpg",
+    "poster_path": "/8cnfdskwEmS1HZhenEVYt9P0IYa.jpg",
+    "genres": [
+      {
+        "id": 878,
+        "name": "خيال علمي"
+      },
+      {
+        "id": 27,
+        "name": "رعب"
+      }
+    ],
+    "title": "The Platform 2",
+    "overview": "بعد أن يفرض قائد غامض قانونه على زنازين عمودية تدار وفق نظام قاسٍ، تحارب وافدة جديدة طريقة توزيع الطعام المريبة هناك.",
+    "cast": [
+      {
+        "name": "Milena Smit",
+        "profile_path": "/aD7RhtQZZ6adyPiy4jp3RB4DhEs.jpg"
+      },
+      {
+        "name": "Hovik Keuchkerian",
+        "profile_path": "/o5cGKWHlZl4RFROYlT2hD95j0lN.jpg"
+      },
+      {
+        "name": "Natalia Tena",
+        "profile_path": "/7kN9NpECZoR4NLXb4SlpHOaJx7T.jpg"
+      },
+      {
+        "name": "أوسكار خاينادا",
+        "profile_path": "/qwphNLIwMyQW2ZLA0g0l1zFiB3w.jpg"
+      },
+      {
+        "name": "Ivan Massagué",
+        "profile_path": "/dmnJo2sGlfFjtKb8FDPlNjRhfyc.jpg"
+      }
+    ],
+    "custom_link": "https://w5.gameshop4u.com/embed-embed-umo6mft9uw36.html"
+  }
+];
---
+++ tvshows_database.js
@@ -0,0 +1,296 @@
+ tvshowsDatabase = [
+  {
+    "id": 257812,
+    "original_language": "ar",
+    "release_date": "2024-09-21",
+    "backdrop_path": "/a4s9pS0WRmz5b8K3pJDolDXIgbq.jpg",
+    "poster_path": "/6M3I0mF60Na6nfQGCuU0POs7Dr6.jpg",
+    "genres": [
+      {
+        "id": 35,
+        "name": "كوميديا"
+      },
+      {
+        "id": 18,
+        "name": "دراما"
+      }
+    ],
+    "name": "تيتا زوزو",
+    "overview": "في إطار درامي اجتماعي، تتناول الأحداث قصة الجدة الأرملة زوزو التي كانت تعمل بالزراعة، ولديها ولدان وابنتان، حيث نتابع علاقتها بأحفادها، وتعاملها معهم ومع المشكلات التي يتعرضون لها.",
+    "number_of_seasons": 1,
+    "seasons": [
+      {
+        "id": 399826,
+        "air_date": "2024-09-21",
+        "episode_count": 30,
+        "overview": "في إطار درامي اجتماعي، تتناول الأحداث قصة الجدة الأرملة زوزو التي كانت تعمل بالزراعة، ولديها ولدان وابنتان، حيث نتابع علاقتها بأحفادها، وتعاملها معهم ومع المشكلات التي يتعرضون لها.",
+        "poster_path": null,
+        "season_number": 1,
+        "episodes": [
+          {
+            "episode_number": 1,
+            "name": "الحلقة 1",
+            "air_date": "2024-09-21",
+            "overview": "",
+            "image_url": "/coja80Y3B1XJeCJpW6l1ReCH7Zh.jpg",
+            "custom_link": "https://w5.gameshop4u.com/embed-embed-wrwktsa3aa4d.html"
+          },
+          {
+            "episode_number": 2,
+            "name": "الحلقة 2",
+            "air_date": "2024-09-22",
+            "overview": "",
+            "image_url": "/coja80Y3B1XJeCJpW6l1ReCH7Zh.jpg",
+            "custom_link": "https://w5.gameshop4u.com/embed-embed-xjo18xcrchxn.html"
+          },
+          {
+            "episode_number": 3,
+            "name": "الحلقة 3",
+            "air_date": "2024-09-23",
+            "overview": "",
+            "image_url": "/coja80Y3B1XJeCJpW6l1ReCH7Zh.jpg",
+            "custom_link": "https://w5.gameshop4u.com/embed-nohdwq9ilz3h.html"
+          },
+          {
+            "episode_number": 4,
+            "name": "الحلقة 4",
+            "air_date": "2024-09-24",
+            "overview": "",
+            "image_url": "/coja80Y3B1XJeCJpW6l1ReCH7Zh.jpg",
+            "custom_link": "https://w5.gameshop4u.com/embed-dr3bmcp9fiu9.html"
+          },
+          {
+            "episode_number": 5,
+            "name": "الحلقة 5",
+            "air_date": "2024-09-25",
+            "overview": "",
+            "image_url": "/coja80Y3B1XJeCJpW6l1ReCH7Zh.jpg",
+            "custom_link": "https://w5.gameshop4u.com/embed-9oo6128f1zj3.html"
+          },
+          {
+            "episode_number": 6,
+            "name": "الحلقة 6",
+            "air_date": "2024-09-28",
+            "overview": "",
+            "image_url": "/coja80Y3B1XJeCJpW6l1ReCH7Zh.jpg",
+            "custom_link": "https://w5.gameshop4u.com/embed-yzr9p4i65654.html"
+          },
+          {
+            "episode_number": 7,
+            "name": "الحلقة 7",
+            "air_date": "2024-09-29",
+            "overview": "",
+            "image_url": "/coja80Y3B1XJeCJpW6l1ReCH7Zh.jpg",
+            "custom_link": "https://w5.gameshop4u.com/embed-envxwyih69gp.html"
+          },
+          {
+            "episode_number": 8,
+            "name": "الحلقة 8",
+            "air_date": "2024-09-30",
+            "overview": "",
+            "image_url": "/coja80Y3B1XJeCJpW6l1ReCH7Zh.jpg",
+            "custom_link": "https://w5.gameshop4u.com/embed-riwrnhw8bo7u.html"
+          },
+          {
+            "episode_number": 9,
+            "name": "الحلقة 9",
+            "air_date": "2024-10-01",
+            "overview": "",
+            "image_url": "/coja80Y3B1XJeCJpW6l1ReCH7Zh.jpg",
+            "custom_link": "https://w5.gameshop4u.com/embed-tcsnfzphlk3g.html"
+          },
+          {
+            "episode_number": 10,
+            "name": "الحلقة 10",
+            "air_date": "2024-10-02",
+            "overview": "",
+            "image_url": "/coja80Y3B1XJeCJpW6l1ReCH7Zh.jpg",
+            "custom_link": "https://w5.gameshop4u.com/embed-z2yyk6rlrc25.html"
+          },
+          {
+            "episode_number": 11,
+            "name": "الحلقة 11",
+            "air_date": "2024-10-05",
+            "overview": "",
+            "image_url": "/coja80Y3B1XJeCJpW6l1ReCH7Zh.jpg",
+            "custom_link": "https://w5.gameshop4u.com/embed-tca7yynsbz4l.html"
+          },
+          {
+            "episode_number": 12,
+            "name": "الحلقة 12",
+            "air_date": "2024-10-06",
+            "overview": "",
+            "image_url": "/coja80Y3B1XJeCJpW6l1ReCH7Zh.jpg",
+            "custom_link": "https://w5.gameshop4u.com/embed-sg5yzy2zxe6k.html"
+          },
+          {
+            "episode_number": 13,
+            "name": "الحلقة 13",
+            "air_date": "2024-10-07",
+            "overview": "",
+            "image_url": "/coja80Y3B1XJeCJpW6l1ReCH7Zh.jpg",
+            "custom_link": "https://w5.gameshop4u.com/embed-iu7c7yeljn2e.html"
+          },
+          {
+            "episode_number": 14,
+            "name": "الحلقة 14",
+            "air_date": "2024-10-08",
+            "overview": "",
+            "image_url": "/coja80Y3B1XJeCJpW6l1ReCH7Zh.jpg",
+            "custom_link": "https://w5.gameshop4u.com/embed-4kminpfqzn8t.html"
+          },
+          {
+            "episode_number": 15,
+            "name": "الحلقة 15",
+            "air_date": "2024-10-09",
+            "overview": "",
+            "image_url": "/coja80Y3B1XJeCJpW6l1ReCH7Zh.jpg",
+            "custom_link": "https://w5.gameshop4u.com/embed-oy4rvb7pxmn6.html"
+          },
+          {
+            "episode_number": 16,
+            "name": "الحلقة 16",
+            "air_date": "2024-10-12",
+            "overview": "",
+            "image_url": "/coja80Y3B1XJeCJpW6l1ReCH7Zh.jpg",
+            "custom_link": "https://w5.gameshop4u.com/embed-z2k5skww04ry.html"
+          },
+          {
+            "episode_number": 17,
+            "name": "الحلقة 17",
+            "air_date": "2024-10-13",
+            "overview": "",
+            "image_url": "/coja80Y3B1XJeCJpW6l1ReCH7Zh.jpg",
+            "custom_link": "https://w5.gameshop4u.com/embed-xn11n1qtcxvm.html"
+          },
+          {
+            "episode_number": 18,
+            "name": "الحلقة 18",
+            "air_date": "2024-10-14",
+            "overview": "",
+            "image_url": "/coja80Y3B1XJeCJpW6l1ReCH7Zh.jpg",
+            "custom_link": "https://w5.gameshop4u.com/embed-bsbcugo6etrc.html"
+          },
+          {
+            "episode_number": 19,
+            "name": "الحلقة 19",
+            "air_date": "2024-10-15",
+            "overview": "",
+            "image_url": "/coja80Y3B1XJeCJpW6l1ReCH7Zh.jpg",
+            "custom_link": "https://w5.gameshop4u.com/embed-mq1p8cs9xc6q.html"
+          },
+          {
+            "episode_number": 20,
+            "name": "الحلقة 20",
+            "air_date": "2024-10-16",
+            "overview": "",
+            "image_url": "/coja80Y3B1XJeCJpW6l1ReCH7Zh.jpg",
+            "custom_link": "https://w5.gameshop4u.com/embed-o609v6i2ll65.html"
+          },
+          {
+            "episode_number": 21,
+            "name": "الحلقة 21",
+            "air_date": "2024-10-19",
+            "overview": "",
+            "image_url": "/coja80Y3B1XJeCJpW6l1ReCH7Zh.jpg",
+            "custom_link": "https://w5.gameshop4u.com/embed-4ejwx1jhxt95.html"
+          },
+          {
+            "episode_number": 22,
+            "name": "الحلقة 22",
+            "air_date": "2024-10-20",
+            "overview": "",
+            "image_url": "/coja80Y3B1XJeCJpW6l1ReCH7Zh.jpg",
+            "custom_link": "https://w5.gameshop4u.com/embed-g4reit5z2d8a.html"
+          },
+          {
+            "episode_number": 23,
+            "name": "الحلقة 23",
+            "air_date": "2024-10-21",
+            "overview": "",
+            "image_url": "/coja80Y3B1XJeCJpW6l1ReCH7Zh.jpg",
+            "custom_link": "https://w5.gameshop4u.com/embed-s3jae1fx9hft.html"
+          },
+          {
+            "episode_number": 24,
+            "name": "الحلقة 24",
+            "air_date": "2024-10-22",
+            "overview": "",
+            "image_url": "/coja80Y3B1XJeCJpW6l1ReCH7Zh.jpg",
+            "custom_link": "https://w5.gameshop4u.com/embed-uhl2tejzztew.html"
+          },
+          {
+            "episode_number": 25,
+            "name": "الحلقة 25",
+            "air_date": "2024-10-23",
+            "overview": "",
+            "image_url": "/coja80Y3B1XJeCJpW6l1ReCH7Zh.jpg",
+            "custom_link": "https://w5.gameshop4u.com/embed-f2ofv1zu6c5m.html"
+          },
+          {
+            "episode_number": 26,
+            "name": "الحلقة 26",
+            "air_date": "2024-10-26",
+            "overview": "",
+            "image_url": "/coja80Y3B1XJeCJpW6l1ReCH7Zh.jpg",
+            "custom_link": "https://w5.gameshop4u.com/embed-8kqzmcurnozi.html"
+          },
+          {
+            "episode_number": 27,
+            "name": "الحلقة 27",
+            "air_date": "2024-10-27",
+            "overview": "",
+            "image_url": "/coja80Y3B1XJeCJpW6l1ReCH7Zh.jpg",
+            "custom_link": "https://w5.gameshop4u.com/embed-tmaxqp7cmjps.html"
+          },
+          {
+            "episode_number": 28,
+            "name": "الحلقة 28",
+            "air_date": "2024-10-28",
+            "overview": "",
+            "image_url": "/coja80Y3B1XJeCJpW6l1ReCH7Zh.jpg",
+            "custom_link": "https://w5.gameshop4u.com/embed-4g68999j7ufr.html"
+          },
+          {
+            "episode_number": 29,
+            "name": "الحلقة 29",
+            "air_date": "2024-10-29",
+            "overview": "",
+            "image_url": "/coja80Y3B1XJeCJpW6l1ReCH7Zh.jpg",
+            "custom_link": "https://w5.gameshop4u.com/embed-217da3534gc0.html"
+          },
+          {
+            "episode_number": 30,
+            "name": "الحلقة 30",
+            "air_date": "2024-10-30",
+            "overview": "",
+            "image_url": "/coja80Y3B1XJeCJpW6l1ReCH7Zh.jpg",
+            "custom_link": "https://w5.gameshop4u.com/embed-4uxk7qa81q00.html"
+          }
+        ]
+      }
+    ],
+    "cast": [
+      {
+        "name": "إسعاد يونس",
+        "profile_path": "/jP7Sc5tSneEBTmni0npq87obq1g.jpg"
+      },
+      {
+        "name": "ندى موسى",
+        "profile_path": "/lYKQkpiM6XyRUJ2RhBkTxPZvWKc.jpg"
+      },
+      {
+        "name": "محمود البزاوي",
+        "profile_path": "/zSEhTEA07ZE4xchHU5SDwOOytmr.jpg"
+      },
+      {
+        "name": "حمزة العيلي",
+        "profile_path": "/gLkzCbRdV16Ou8xH7RnO3oRk9nd.jpg"
+      },
+      {
+        "name": "نور محمود",
+        "profile_path": "/elAQjszaNcY1XNZbDLMEkePiZh4.jpg"
+      }
+    ]
+  }
+  ];
